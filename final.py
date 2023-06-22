a7517b54e6b69406805fb01f7439584273fe232c
nada khatib <nadakhatib2004@gmail.com>
2023-06-22 20:06:28 +0300

    first commit

--- final.py
+++ final.py
@@ -11,6 +11,22 @@ Delivery Date :22/6/2023
 # course mark (user_input)
 
 
+
+import uuid
+
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()
+        self.course_name = course_name
+        self.course_mark = course_mark
+
+             
+course_name = input("Enter the course name: ")
+course_mark = float(input("Enter the course mark: "))
+
+course = Course(course_name, course_mark)
+
+
 class Student:
     # TODO 3 define static variable indicates total student count
 
